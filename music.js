ec47b40396b812ca92f6f56dc03e0743cd6066ea
ardagnr <arda.fusjn@gmail.com>
2024-01-19 12:58:47 +0300

    Müzik Oynatıcısı için şarkılar ve çeşitli fonksiyonlar eklendi

---
+++ music.js
@@ -0,0 +1,17 @@
+class Music {
+  constructor(title, singer, img, file) {
+    this.title = title;
+    this.singer = singer;
+    this.img = img;
+    this.file = file;
+  }
+  getName() {
+    return this.title + "-" + this.singer;
+  }
+}
+
+const musicList = [
+  new Music("Affet", "Müslüm Gürses", "1.jpeg", "1.mp3"),
+  new Music("Delikanlım", "Yıldız Tilbe", "2.jpeg", "2.mp3"),
+  new Music("Aramızda Uçurumlar", "Suat Suna", "3.jpeg", "3.mp3"),
+];
